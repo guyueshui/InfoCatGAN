1abd8ade05fc3ec8bbffa572c6fe214bda865d49
guyueshui <guyueshui002@gmail.com>
2020-06-30 10:16:24 +0800

    ready to push remote

--- models/mnist.py
+++ models/mnist.py
@@ -1,116 +1,82 @@
 from . import *
 from torch.nn.utils import weight_norm
 
-class Classifier(nn.Module):
-    def __init__(self, in_dim, out_dim):
-        super(Classifier, self).__init__()
-        # torch.Size([bs, 1, 28, 28])
-        layers = []
-        layers += [
-            ReshapeLayer((-1, 1, 28, 28)),
-            CovvLayer(in_dim, 32, 5, 1, 0, act=nn.ReLU(), bn=True, ps=2, dr=0.5),   # 32, 12, 12
-            CovvLayer(32, 64, 3, 1, 1, act=nn.ReLU(), bn=True, ps=1, dr=0),         # 64, 12, 12
-            CovvLayer(64, 64, 3, 1, 1, act=nn.ReLU(), bn=True, ps=2, dr=0.5),       # 64, 6, 6
-            CovvLayer(64, 128, 3, 1, 1, act=nn.ReLU(), bn=True, ps=1, dr=0),        # 128, 6, 6
-            CovvLayer(128, 128, 3, 1, 0, act=nn.ReLU(), bn=True, ps=1, dr=0),       # 128, 4, 4
-            nn.AvgPool2d(4)
-        ]
-        self.conv = nn.Sequential(*layers)
-        self.fc = nn.Sequential(
-            nn.Linear(128, out_dim),
-            nn.Softmax(dim=1),  # Each row sums to 1.
+#========= following arch from =============
+# https://github.com/pianomania/infoGAN-pytorch/blob/master/model.py
+class PianFrontD(nn.Module):
+    ''' front end part of discriminator and Q'''
+    def __init__(self, in_dim=1, out_dim=1024):
+        super(PianFrontD, self).__init__()
+
+        self.main = nn.Sequential(
+          nn.Conv2d(in_dim, 64, 4, 2, 1),           # 28x28 -> 14x14
+          nn.LeakyReLU(0.1, inplace=True),
+          nn.Conv2d(64, 128, 4, 2, 1, bias=False),  # 14x14 -> 7x7
+          nn.BatchNorm2d(128),
+          nn.LeakyReLU(0.1, inplace=True),
+          nn.Conv2d(128, out_dim, 7, bias=False),      # 7x7 -> 1x1
+          nn.BatchNorm2d(out_dim),
+          nn.LeakyReLU(0.1, inplace=True),
         )
-    
+
     def forward(self, x):
-        x = self.conv(x).squeeze()
-        x = self.fc(x)
-        return x
+        output = self.main(x)
+        return output
 
 
-class Generator(nn.Module):
-    def __init__(self, in_dim, extra_dim=10, out_dim=28*28):
-        super(Generator, self).__init__()
-        self.blk1 = self._f_(in_dim + extra_dim, 500)
-        self.blk2 = self._f_(500 + extra_dim, 500)
-        self.blk3 = nn.Sequential(
-            nn.Linear(500 + extra_dim, out_dim),
-            nn.Sigmoid(),
-            weight_norm(nn.BatchNorm1d(out_dim))
-        )
-    
-    def _f_(self, in_dim, out_dim):
-        seq = nn.Sequential(
-            nn.Linear(in_dim, out_dim),
-            nn.Softplus(),
-            nn.BatchNorm1d(out_dim)
-        )
-        return seq
-    
-    def forward(self, zac, y_g):
-        assert zac.dim() == 2, "undesired input shape"
-        if y_g.dim() < 2:
-            y_g = nn.functional.one_hot(y_g.long(), num_classes=10)
-            y_g = y_g.float()
-        x = torch.cat([zac, y_g], dim=1)
-        x = torch.cat([self.blk1(x), y_g], dim=1)
-        x = torch.cat([self.blk2(x), y_g], dim=1)
-        x = self.blk3(x).view(-1, 1, 28, 28)
-        return x
-        
-class Discriminator(nn.Module):
-    def __init__(self, in_dim=28*28, extra_dim=10, out_dim=1):
-        super(Discriminator, self).__init__()
-        self.nl_data = GaussianNoiseLayer(0.3)
-        self.nl = GaussianNoiseLayer(0.5)
-        self.reshape = ReshapeLayer((-1, in_dim))
-        self.fc1 = self._f_(in_dim + extra_dim, 1000)
-        self.fc2 = self._f_(1000 + 2*extra_dim, 500)
-        self.fc3 = self._f_(500 + extra_dim, 250)
-        self.fc4 = self._f_(250 + extra_dim, 250)
-        self.fc5 = self._f_(250 + extra_dim, 250)
-        self.fc6 = nn.Sequential(
-            weight_norm(nn.Linear(250 + extra_dim, 1)),
-            nn.Sigmoid(),
+class PianD(nn.Module):
+    def __init__(self, in_dim=1024, out_dim=1):
+        super(PianD, self).__init__()
+        self.main = nn.Sequential(
+          nn.Conv2d(in_dim, out_dim, 1),
+          nn.Sigmoid()
         )
 
-    def _f_(self, in_dim, out_dim):
-        seq = nn.Sequential(
-            weight_norm( nn.Linear(in_dim, out_dim) ),
-            nn.ReLU()
+    def forward(self, x):
+        output = self.main(x).view(-1, 1)
+        return output
+
+
+class PianQ(nn.Module):
+    def __init__(self, in_dim=1024, cat_dim=10, num_cont_code=2):
+        super(PianQ, self).__init__()
+        self.conv = nn.Conv2d(in_dim, 128, 1, bias=False)
+        self.bn = nn.BatchNorm2d(128)
+        self.lReLU = nn.LeakyReLU(0.1, inplace=True)
+        self.conv_disc = nn.Conv2d(128, cat_dim, 1, bias=False)
+        self.conv_mu = nn.Conv2d(128, num_cont_code, 1, bias=False)
+        self.conv_var = nn.Conv2d(128, num_cont_code, 1, bias=False)
+
+    def forward(self, x):
+        y = self.lReLU(self.bn(self.conv(x)))
+        disc_logits = self.conv_disc(y).squeeze()
+        mu = self.conv_mu(y).squeeze()
+        var = self.conv_var(y).squeeze().exp()
+        return disc_logits, mu, var 
+
+
+class PianG(nn.Module):
+    def __init__(self, in_dim, out_dim):
+        super(PianG, self).__init__()
+        self.in_dim = in_dim
+        self.main = nn.Sequential(
+          nn.ConvTranspose2d(in_dim, 1024, 1, 1, bias=False),
+          nn.BatchNorm2d(1024),
+          nn.ReLU(True),
+          nn.ConvTranspose2d(1024, 128, 7, 1, bias=False),
+          nn.BatchNorm2d(128),
+          nn.ReLU(True),
+          nn.ConvTranspose2d(128, 64, 4, 2, 1, bias=False),
+          nn.BatchNorm2d(64),
+          nn.ReLU(True),
+          nn.ConvTranspose2d(64, out_dim, 4, 2, 1, bias=False),
+          nn.Sigmoid()
         )
-        return seq
-    
-    def forward(self, x, y):
-        if y.dim() < 2:
-            y = nn.functional.one_hot(y.long(), num_classes=10)
-            y = y.float()
-        x = self.reshape(x)
-        x = self.nl_data(x)
-        x = torch.cat([x, y], dim=1)
 
-        x = torch.cat([self.fc1(x), y], dim=1)
-        x = self.nl(x)
-        x = torch.cat([x, y], dim=1)
-
-        x = self.fc2(x)
-        x = self.nl(x)
-        x = torch.cat([x, y], dim=1)
-
-        x = self.fc3(x)
-        x = self.nl(x)
-        x = torch.cat([x, y], dim=1)
-
-        x = self.fc4(x)
-        x = self.nl(x)
-        x = torch.cat([x, y], dim=1)
-
-        x = self.fc5(x)
-        x = self.nl(x)
-        x = torch.cat([x, y], dim=1)
-
-        x = self.fc6(x)
-        return x
-
+    def forward(self, x):
+        x = x.view(-1, self.in_dim, 1, 1)
+        output = self.main(x)
+        return output
 
 #=========== following arch from CatGAN paper ===========
 """
@@ -214,74 +180,122 @@ class G(nn.Module):
 
 #====== following arch from InfoGAN paper =======
 class InfoGAN_G(nn.Module):
-  'Arch from InfoGAN paper.'
-  def __init__(self, in_dim=74, out_dim=1):
-      super(InfoGAN_G, self).__init__()
+    'Arch from InfoGAN paper.'
+    def __init__(self, in_dim=74, out_dim=1):
+        super(InfoGAN_G, self).__init__()
 
-      # bs x input_dim
-      self.fc = nn.Sequential(
-        nn.Linear(in_dim, 1024),  # bs x 1024
-        nn.BatchNorm1d(1024),
-        nn.ReLU(),
-        nn.Linear(1024, 128 * 7 * 7), # bs x 128*7*7
-        nn.BatchNorm1d(128 * 7 * 7),
-        nn.ReLU()
-      )
+        # bs x input_dim
+        self.fc = nn.Sequential(
+          nn.Linear(in_dim, 1024),  # bs x 1024
+          nn.BatchNorm1d(1024),
+          nn.ReLU(),
+          nn.Linear(1024, 128 * 7 * 7), # bs x 128*7*7
+          nn.BatchNorm1d(128 * 7 * 7),
+          nn.ReLU()
+        )
 
-      # torch.Size([bs, 128, 7, 7])
-      self.deconv = nn.Sequential(
-        nn.ConvTranspose2d(128, 64, 4, 2, 1), # bs x 64 x 14 x 14
-        nn.BatchNorm2d(64),
-        nn.ReLU(),
-        nn.ConvTranspose2d(64, out_dim, 4, 2, 1), # bs x output_dim x 28 x 28
-        nn.Tanh()
-      )
-      # torch.Size([bs, 1, 28, 28])
+        # torch.Size([bs, 128, 7, 7])
+        self.deconv = nn.Sequential(
+          nn.ConvTranspose2d(128, 64, 4, 2, 1), # bs x 64 x 14 x 14
+          nn.BatchNorm2d(64),
+          nn.ReLU(),
+          nn.ConvTranspose2d(64, out_dim, 4, 2, 1), # bs x output_dim x 28 x 28
+          nn.Tanh()
+        )
+        # torch.Size([bs, 1, 28, 28])
 
-  def forward(self, x):
-      x = self.fc(x).view(-1, 128, 7, 7)
-      x = self.deconv(x)
-      return x
+    def forward(self, x):
+        x = self.fc(x).view(-1, 128, 7, 7)
+        x = self.deconv(x)
+        return x
 
-class InfoGAN_D(nn.Module):
-    def __init__(self, in_dim, out_dim, nclass, nconcode):
-        super(InfoGAN_D, self).__init__()
+
+class InfoGAN_Dbody(nn.Module):
+    'Arch from InfoGAN paper.'
+    def __init__(self, in_dim, out_dim=1024):
+        super(InfoGAN_Dbody, self).__init__()
+
         # in_dim x 28 x 28
         self.conv = nn.Sequential(
-            nn.Conv2d(in_dim, 64, 4, 2, 1),  # 64 x 14 x 14
-            nn.LeakyReLU(0.2),
-            nn.Conv2d(64, 128, 4, 2, 1),  # 128 x 7 x 7
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.2)
+          nn.Conv2d(in_dim, 64, 4, 2, 1),  # 64 x 14 x 14
+          nn.LeakyReLU(0.2),
+          nn.Conv2d(64, 128, 4, 2, 1),  # 128 x 7 x 7
+          nn.BatchNorm2d(128),
+          nn.LeakyReLU(0.2)
         )
 
         self.fc = nn.Sequential(
-            nn.Linear(128*7*7, 1024),
-            nn.BatchNorm1d(1024),
-            nn.LeakyReLU(0.2)
+          nn.Linear(128*7*7, 1024),
+          nn.BatchNorm1d(1024),
+          nn.LeakyReLU(0.2),
         )
 
-        self.d = nn.Sequential(
-            nn.Linear(1024, 1),
-            nn.Sigmoid(),
-        )
-
-        self.q = nn.Sequential(
-            nn.Linear(1024, 128),
-            nn.BatchNorm1d(128),
-            nn.LeakyReLU(0.2),
-        )
-
-        self.qdis = nn.Linear(128, nclass)
-        self.qmu = nn.Linear(128, nconcode)
-        self.qvar = nn.Linear(128, nconcode)
-    
     def forward(self, x):
         x = self.conv(x).view(-1, 128*7*7)
         x = self.fc(x)
-        prob = self.d(x)
-        qstuff = self.q(x)
-        dis_logits = self.qdis(qstuff)
-        qmu = self.qmu(qstuff)
-        qvar = self.qvar(qstuff).exp()
-        return prob, dis_logits, (qmu, qvar)
+        return x
+
+
+class InfoGAN_Dhead(nn.Module):
+    def __init__(self, in_dim, out_dim):
+        super(InfoGAN_Dhead, self).__init__()
+        self.main = nn.Sequential(
+          nn.Linear(in_dim, out_dim),
+          nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = self.main(x)
+        return x
+
+
+class InfoGAN_Qhead(nn.Module):
+    def __init__(self, latent_dim, dis_dim=10, con_dim=2):
+        super(InfoGAN_Qhead, self).__init__()
+        self.fc = nn.Sequential(
+          nn.Linear(latent_dim, 128),
+          nn.BatchNorm1d(128),
+          nn.LeakyReLU(0.2),
+          # nn.Linear(128, self.dis_dim + self.con_dim)
+        )
+
+        self.disc = nn.Linear(128, dis_dim)
+        self.mu = nn.Linear(128, con_dim)
+        self.var = nn.Linear(128, con_dim)
+
+    def forward(self, x):
+        x = self.fc(x)
+        disc_logits = self.disc(x)
+        mu = self.mu(x)
+        var = self.var(x).exp()
+        return disc_logits, mu, var
+
+
+class InfoGAN_CatD(nn.Module):
+    'Arch from InfoGAN paper.'
+    def __init__(self, in_dim, out_dim):
+        super(InfoGAN_CatD, self).__init__()
+
+        # in_dim x 28 x 28
+        self.conv = nn.Sequential(
+          nn.Conv2d(in_dim, 64, 4, 2, 1),  # 64 x 14 x 14
+          nn.LeakyReLU(0.2),
+          nn.Conv2d(64, 128, 4, 2, 1),  # 128 x 7 x 7
+          nn.BatchNorm2d(128),
+          nn.LeakyReLU(0.2)
+        )
+
+        self.fc = nn.Sequential(
+          nn.Linear(128*7*7, 1024),
+          nn.BatchNorm1d(1024),
+          nn.LeakyReLU(0.2),
+          nn.Linear(1024, out_dim),
+        )
+
+        self.softmax = nn.Softmax(dim=1) # Each row sums to 1.
+
+    def forward(self, x):
+        x = self.conv(x).view(-1, 128*7*7)
+        logits = self.fc(x)
+        simplex = self.softmax(logits)
+        return simplex, logits
